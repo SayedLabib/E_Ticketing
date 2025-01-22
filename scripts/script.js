f0a97b939311a4ea420eb5b3f85ef5f6c2c840cc
Sayed Labib <98503095+SayedLabib@users.noreply.github.com>
2025-01-22 22:34:39 +0600

    The seat booking system is created

--- scripts/script.js
+++ scripts/script.js
@@ -2,15 +2,28 @@
 
 const allSeats = document.getElementsByClassName('kbd-lg'); 
 
+let seatCount = 0;
+
 for(const seats of allSeats)
     {
        seats.addEventListener('click', function(event) {
+
+
+      if(event.target.classList.contains('disabled'))
+         {
+           alert('The seat is already booked please choose another one');
+           return;
+         }
+
     
        const seat = event.target.innerText;
        const seatClass = 'Economy';
        const price = 550;
-    //    console.log(seatClass);
-
+      
+     
+      if(seatCount < 4)
+      {
+               
        const selectedSeats = document.getElementById('selected-seats');
 
        const div = document.createElement('div');
@@ -29,9 +42,26 @@ for(const seats of allSeats)
        div.appendChild(p1);
        div.appendChild(p2);
        div.appendChild(p3);
-       
-       selectedSeats.appendChild(div);
 
+       selectedSeats.appendChild(div); 
+
+       seatCount++;
+      
+       event.target.classList.add('disabled');
+      //  event.target.style.pointerEvents = 'none';
+       event.target.style.backgroundColor = '#1DD100';
+
+      }
+
+            
+      
+      
+      UpdateSeatCount();
+      UpdateSeatLeft();
+       
+
+       
+       
 
        })
     }
--- scripts/utility.js
+++ scripts/utility.js
@@ -1,1 +1,27 @@
 
+function getConvertedValue(value) 
+{
+    const Value = document.getElementById(value).innerText;
+    const getConvertedValue = parseInt(Value);
+    return getConvertedValue;
+}
+
+function UpdateSeatCount()
+{
+    const count = getConvertedValue('seat-count');
+    
+    if(count+1>4)
+        {
+          alert('You have reached your buy limit');
+          return;
+        }
+    
+    document.getElementById('seat-count').innerText = count + 1   
+
+}
+
+function UpdateSeatLeft()
+{
+    const seatLeft = getConvertedValue('seat-left');
+    document.getElementById('seat-left').innerText = seatLeft - 1;
+}
